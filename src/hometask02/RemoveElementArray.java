1c40210e7114ff65dd19fdd9220a08d4aef347c3
B.Yevhen <y.bulatovych@gmail.com>
2019-11-12 17:59:47 +0200

    ht2t2

--- src/hometask02/RemoveElementArray.java
+++ src/hometask02/RemoveElementArray.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class RemoveElementArray {
     public static void main(String[] args) {
 
-        int[] ints = {0, 1, 2, 2, 3, 0, 4, 2};
+        int[] ints = {0, 1, 2, 2, 3, 0, 4, 0};
         int val = 2;
         System.out.println(removeElement(ints, val));
 
@@ -14,19 +14,17 @@ public class RemoveElementArray {
     public static int removeElement(int[] ints, int val) {
         int newLength = ints.length;
 
-        if (ints[ints.length - 1] == val) {
-            ints[ints.length - 1] = 0; // чтобы не попасть в бесконеный цикл
-            newLength--;
-        }
         for (int i = 0; i < ints.length; i++) {
             if (ints[i] == val) {
                 for (int j = i; j < (ints.length - 1); j++) {
                     ints[j] = ints[j + 1];
                 }
+                ints[ints.length - 1] = 0;
                 newLength--;
                 i--;
             }
         }
+
         System.out.println(Arrays.toString(ints));
 
         return newLength;
