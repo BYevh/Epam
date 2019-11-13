2dacac8d348c0796870bf8649599927e5cd92934
B.Yevhen <y.bulatovych@gmail.com>
2019-11-13 19:28:37 +0200

    added string constants and braces

--- src/hometask01/GuessingGame.java
+++ src/hometask01/GuessingGame.java
@@ -6,26 +6,36 @@ import java.io.InputStreamReader;
 import java.util.Random;
 
 public class GuessingGame {
+
+    private static final String ENTER = "Guess the number from 1 to 1000: ";
+    private static final String LOW = "Too low.\nTry again:";
+    private static final String HIGH = "Too high.\nTry again:";
+    private static final String TRUE = "Yes!";
+    private static final String ERROR = "Only number.\nTry again:";
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int random = new Random().nextInt(1000);
         System.out.println(random);
-        System.out.println("Guess the number from 1 to 1000: ");
+        System.out.println(ENTER);
         int  number = 0;
 
         do {
             try {
                 number = Integer.parseInt(reader.readLine());
             } catch (NumberFormatException e) {
-                System.out.println("Only number.\nTry again:");
+                System.out.println(ERROR);
                 continue;
             }
-            if (number < random) System.out.println("Too low.\nTry again:");
-            else if (number > random) System.out.println("Too high.\nTry again:");
+            if (number < random) {
+                System.out.println(LOW);
+            } else if (number > random) {
+                System.out.println(HIGH);
+            }
 
         } while (number != random);
 
-        System.out.println("Yes!");
+        System.out.println(TRUE);
 
         reader.close();
     }
