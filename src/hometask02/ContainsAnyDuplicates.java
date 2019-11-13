e1cb3a168029b13abdc53d03c921e8a224eff378
B.Yevhen <y.bulatovych@gmail.com>
2019-11-13 09:43:38 +0200

    ht2t5

---
+++ src/hometask02/ContainsAnyDuplicates.java
@@ -0,0 +1,21 @@
+package hometask02;
+
+public class ContainsAnyDuplicates {
+
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 3, 4};
+        System.out.println(duplicates(ints));
+    }
+
+    public static boolean duplicates(int[] ints) {
+        for (int i = 0; i < ints.length - 1; i++) {
+            for (int j = i + 1; j < ints.length; j++) {
+                if (ints[i] == ints[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
