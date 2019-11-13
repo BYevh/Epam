d1b20416ab56f3ade0186f38e9eb3f5a9fde5325
B.Yevhen <y.bulatovych@gmail.com>
2019-11-13 08:47:58 +0200

    ht2t4

---
+++ src/hometask02/PlusOneToArray.java
@@ -0,0 +1,32 @@
+package hometask02;
+
+import java.util.Arrays;
+
+public class PlusOneToArray {
+
+    public static void main(String[] args) {
+        int [] ints = {9,9,9,9,9};
+        System.out.println(Arrays.toString(plusToArray(ints)));
+    }
+
+    public static int[] plusToArray (int[]ints){
+        int i = 1;
+        int number = 1;
+
+        ints[ints.length-1] = ints[ints.length-1] + number;
+        while ((ints[ints.length-i] > 9) & (i < ints.length)){
+            ints[ints.length-i] = 0;
+            i++;
+            ints[ints.length-i] = ints[ints.length-i] + 1;
+        }
+        if (ints[0] > 9){
+            int[] newInts = new int[ints.length+1];
+            newInts[0] = 1;
+            System.arraycopy(newInts, 1, ints, 0, ints.length);
+            return newInts;
+        }
+
+        return ints;
+    }
+
+}
