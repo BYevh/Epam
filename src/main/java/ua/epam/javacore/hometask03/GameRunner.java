6cc64d2a42ebb61005d03a431ef8c938dcd1271c
B.Yevhen <y.bulatovych@gmail.com>
2019-11-21 08:24:58 +0200

    moved all from gamerunner

--- src/main/java/ua/epam/javacore/hometask03/GameRunner.java
+++ src/main/java/ua/epam/javacore/hometask03/GameRunner.java
@@ -3,33 +3,9 @@ package ua.epam.javacore.hometask03;
 import java.util.Scanner;
 
 public class GameRunner {
-    private static final String GREETINGS = "Hi all! It's a game: ";
-    private static final String NAME_OF_GAME = "\"Rock, Paper or Scissors\"";
-    private static final String INPUT_NAME = "\nInput your Name: ";
-    private static final String PLAYER2_NAME = "Computer";
-
-
 
     public static void main(String[] args) {
-        System.out.println(GREETINGS + NAME_OF_GAME + INPUT_NAME);
-        Scanner scanner = new Scanner(System.in);
-        Player player1 = new Player(scanner.nextLine());
-        Player player2 = new Player(PLAYER2_NAME);
         Logic newGame = new Logic();
-
-        while (true) {
-            int number = newGame.number();                                              // input throw number
-            if (number == 0) break;                                                     // 0 - exit
-
-            player1.setNameOfResult(number);                                            //enter the entered result
-            player2.setNameOfResult(newGame.diceRoll());                                //enter the random result
-
-            System.out.println(player1.getName() + " : " + player1.getNameOfResult());  // display throws on the screen
-            System.out.println(player2.getName() + " : " + player2.getNameOfResult());
-            System.out.println(newGame.resultOfRolls(player1, player2));                // determine the winner
-        }
-
-        scanner.close();
-
+        newGame.startGame();
     }
 }
--- src/main/java/ua/epam/javacore/hometask03/Logic.java
+++ src/main/java/ua/epam/javacore/hometask03/Logic.java
@@ -6,17 +6,39 @@ import java.util.Scanner;
 public class Logic {
 
     private String result;
-
-
+    private static final String GREETINGS = "Hi all! It's a game: ";
+    private static final String NAME_OF_GAME = "\"Rock, Paper or Scissors\"";
+    private static final String INPUT_NAME = "\nInput your Name: ";
+    private static final String PLAYER2_NAME = "Computer";
     private static final String INPUT_ANSWER = "\nInput your number (1-rock / 2-paper / 3-scissors / 0 - exit):";
     private static final String WINNER = "Winner: ";
     private static final String MASSAGE_TIE = "A tie!";
-    private static final String MASSAGE_ERROR1 = "That not a number!";
+    private static final String MASSAGE_ERROR1 = "Only number!";
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void startGame(){
+        System.out.println(GREETINGS + NAME_OF_GAME + INPUT_NAME);
+
+        Player player1 = new Player(scanner.nextLine());
+        Player player2 = new Player(PLAYER2_NAME);
+
+        while (true) {
+            int number = number();                                              // input throw number
+            if (number == 0) break;                                                     // 0 - exit
+
+            player1.setNameOfResult(number);                                            //enter the entered result
+            player2.setNameOfResult(diceRoll());                                //enter the random result
+
+            System.out.println(player1.getName() + " : " + player1.getNameOfResult());  // display throws on the screen
+            System.out.println(player2.getName() + " : " + player2.getNameOfResult());
+            System.out.println(resultOfRolls(player1, player2));                // determine the winner
+        }
+    }
 
 
     //input and validation
     public int number(){
-        Scanner scanner = new Scanner(System.in);
         int number;
         do {
             System.out.println(INPUT_ANSWER);
@@ -25,8 +47,7 @@ public class Logic {
                 scanner.next();
             }
             number = scanner.nextInt();
-        }
-        while ( number < 1 & number > 3);
+        } while ( number < 1 | number > 3);
         return number;
     }
 
