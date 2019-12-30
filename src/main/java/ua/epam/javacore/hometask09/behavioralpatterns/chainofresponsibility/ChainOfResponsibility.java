31d85cf31bcf2015778195857e679427684debc0
B.Yevhen <y.bulatovych@gmail.com>
2019-12-30 21:03:07 +0200

    chain

--- src/main/java/ua/epam/javacore/hometask09/behavioralpatterns/chainofresponsibility/ChainOfResponsibility.java
+++ src/main/java/ua/epam/javacore/hometask09/behavioralpatterns/chainofresponsibility/ChainOfResponsibility.java
@@ -1,4 +1,11 @@
 package ua.epam.javacore.hometask09.behavioralpatterns.chainofresponsibility;
 
 public class ChainOfResponsibility {
+    public static void main(String[] args) {
+        MessagePrinter messagePrinter = new ConsoleMessagePrinter();
+        FileMessagePrinter fileMessagePrinter = new FileMessagePrinter();
+        messagePrinter.setNexMessagePrinter(fileMessagePrinter);
+        fileMessagePrinter.setNexMessagePrinter(new DataBaseMessagePrinter());
+        messagePrinter.print("Hello All");
+    }
 }
